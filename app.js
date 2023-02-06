27dd859470699558dec8ac7083da56e6c7edcb54
Pronay Dutta <duttapronay25@gmail.com>
2023-02-07 02:47:26 +0530

    Initial Commit

---
+++ app.js
@@ -0,0 +1,24 @@
+
+window.onload = function () {
+    var yesBtn = document.getElementById("yes");
+    var noBtn = document.getElementById("no");
+
+    // rest of your code goes here
+    // const noBtn = document.getElementById("no");
+    // const yesBtn = document.getElementById("yes");
+
+    yesBtn.addEventListener("click", function () {
+        noBtn.style.left = "";
+        noBtn.style.top = "";
+        window.location.href = "./response.html";
+    });
+
+    noBtn.addEventListener("mouseover", function () {
+        const maxX = 500;
+        const maxY = 500;
+        const randomX = Math.random() * maxX;
+        const randomY = Math.random() * maxY;
+        noBtn.style.left = randomX + "px";
+        noBtn.style.top = randomY + "px";
+    });
+};
