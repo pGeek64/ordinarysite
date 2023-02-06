915de553b692b9b64559cefed324545ef389aa4e
Pronay Dutta <duttapronay25@gmail.com>
2023-02-07 03:34:48 +0530

    updated app.js

--- app.js
+++ app.js
@@ -16,10 +16,13 @@ window.onload = function () {
     noBtn.addEventListener("mouseover", function () {
         const maxX = 500;
         const maxY = 500;
-        const randomX = Math.random() * maxX;
+        const randomX = 50 % - maxX / 2 + Math.floor(Math.random() * maxX);
         const randomY = Math.random() * maxY;
-        noBtn.style.left = randomX + "px";
+        // left: calc(50% - 250px + Math.floor(Math.random() * 500) + "px");
+        // top: Math.floor(Math.random() * 500) + "px";
+        // noBtn.style.left = randomX + "px";
         noBtn.style.top = randomY + "px";
+        noBtn.style.right = randomX + "px";
         // noBtn.style.left = Math.random() * window.innerWidth + "px";
         // noBtn.style.top = Math.random() * window.innerHeight + "px";
     });
